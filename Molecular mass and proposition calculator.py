47966c08f046c6b96e6f476c2f38d4f70c8df8b5
R Saivenkat <158982097+Saivenkat-206@users.noreply.github.com>
2024-12-16 08:32:13 +0530

    Add files via upload

---
+++ Molecular mass and proposition calculator.py
@@ -0,0 +1,229 @@
+elements = {"O":["Oxygen", 16.00], "H":["Hydrogen", 1.008], "C":["Carbon", 12.01]}
+elements.update({"He":["Helium", 4.003]})
+elements.update({"Li":["Lithium", 6.941]})
+elements.update({"Be":["Beryllium", 9.012]})
+elements.update({"B":["Boron", 10.81]})
+elements.update({"N":["Nitrogen", 14.01]})
+elements.update({"F":["Fluorine", 19.00]})
+elements.update({"Ne":["Neon", 20.18]})
+elements.update({"Na":["Sodium", 22.99]})
+elements.update({"Mg":["Magnesium", 24.31]})
+elements.update({"Al":["Aluminum", 26.98]})
+elements.update({"Si":["Silicon", 28.09]})
+elements.update({"P":["Phosphorus", 30.97]})
+elements.update({"S":["Sulfur", 32.07]})
+elements.update({"Cl":["Chlorine", 35.45]})
+elements.update({"Ar":["Argon", 39.95]})
+elements.update({"K":["Potassium", 39.10]})
+elements.update({"Ca":["Calcium", 40.08]})
+elements.update({"Sc":["Scandium", 44.96]})
+elements.update({"Ti":["Titanium", 47.87]})
+elements.update({"V":["Vanadium", 50.94]})
+elements.update({"Cr":["Chromium", 52.00]})
+elements.update({"Mn":["Manganese", 54.94]})
+elements.update({"Fe":["Iron", 55.85]})
+elements.update({"Co":["Cobalt", 58.93]})
+elements.update({"Ni":["Nickel", 58.69]})
+elements.update({"Cu":["Copper", 63.55]})
+elements.update({"Zn":["Zinc", 65.39]})
+elements.update({"Ga":["Gallium", 69.72]})
+elements.update({"Ge":["Germamium", 72.61]})
+elements.update({"As":["Arsenic", 74.92]})
+elements.update({"Se":["Selenium", 78.96]})
+elements.update({"Br":["Bromine", 79.90]})
+elements.update({"Kr":["Krypton", 83.8]})
+elements.update({"Rb":["Rubidium", 85.47]})
+elements.update({"Sr":["Strontium", 87.62]})
+elements.update({"Y":["Yttrium", 88.91]})
+elements.update({"Zr":["Zirconium", 91.22]})
+elements.update({"Nb":["Niobium", 92.91]})
+elements.update({"Mo":["Molybdenum", 95.94]})
+elements.update({"Tc":["Technetium", 98]})
+elements.update({"Ru":["Ruthenium", 101.17]})
+elements.update({"Rh":["Rhodium", 102.91]})
+elements.update({"Pd":["Palladium", 106.42]})
+elements.update({"Ag":["Silver", 107.87]})
+elements.update({"Cd":["Cadmium", 112.41]})
+elements.update({"In":["Indium", 114.82]})
+elements.update({"Sn":["Tin", 118.71]})
+elements.update({"Sb":["Antimony", 121.76]})
+elements.update({"Te":["Tellurium", 127.60]})
+elements.update({"I":["Iodine", 126.90]})
+elements.update({"Xe":["Xenon", 131.29]})
+elements.update({"Cs":["Cesium", 132.91]})
+elements.update({"Ba":["Barium", 137.38]})
+elements.update({"La":["Lanthanum", 138.91]})
+elements.update({"Hf":["Hafnium", 178.49]})
+elements.update({"Ta":["Tantalum", 180.95]})
+elements.update({"W":["Tungsten", 183.84]})
+elements.update({"Re":["Rhenium", 186.21]})
+elements.update({"Os":["Osmium", 190.23]})
+elements.update({"Ir":["Iridium", 192.22]})
+elements.update({"Pt":["Platnium", 195.08]})
+elements.update({"Au":["Gold", 196.97]})
+elements.update({"Hg":["Mercury", 200.59]})
+elements.update({"Tl":["Thallium", 204.38]})
+elements.update({"Pb":["Lead", 207.20]})
+elements.update({"Bi":["Bismuth", 208.98]})
+elements.update({"Po":["Polonium", 209]})
+elements.update({"At":["Astatine", 210]})
+elements.update({"Rn":["Radon", 222]})
+elements.update({"Fr":["Francium", 223]})
+elements.update({"Ra":["Radium", 226]})
+elements.update({"Ac":["Acitinium", 227]})
+elements.update({"Rf":["Rutherfordium", 261]})
+elements.update({"Db":["Dubnium", 262]})
+elements.update({"Sg":["Seaborgium", 266]})
+elements.update({"Bh":["Bohrium", 264]})
+elements.update({"Hs":["Hassium", 269]})
+elements.update({"Mt":["Meitnerium", 268]})
+elements.update({"Ds":["Darmstadtium", 271]})
+elements.update({"Rg":["Roentgenium", 272]})
+elements.update({"Cn":["Copernicium", 277]})
+elements.update({"Ce":["Cerium", 140.12]})
+elements.update({"Pr":["Praseodymium", 140.91]})
+elements.update({"Nd":["Neodymium", 144.24]})
+elements.update({"Pm":["Promethium", 145]})
+elements.update({"Sm":["Samarium", 150.36]})
+elements.update({"Eu":["Europium", 151.96]})
+elements.update({"Gd":["Gadolinium", 157.25]})
+elements.update({"Tb":["Terbium", 158.93]})
+elements.update({"Dy":["Dysprosium", 162.50]})
+elements.update({"Ho":["Holmium", 164.93]})
+elements.update({"Er":["Erbium", 167.26]})
+elements.update({"Tm":["Thulium", 168.93]})
+elements.update({"Yb":["Ytterbium", 173.08]})
+elements.update({"Lu":["Lutetium", 174.97]})
+elements.update({"Th":["Thorium", 232.04]})
+elements.update({"Pa":["Protactinium", 231.04]})
+elements.update({"U":["Uranium", 238.04]})
+elements.update({"Np":["Neptunium", 237]})
+elements.update({"Pu":["Plutonium", 244]})
+elements.update({"Am":["Americium", 243]})
+elements.update({"Cm":["Curium", 247]})
+elements.update({"Bk":["Berkelium", 247]})
+elements.update({"Cf":["Californium", 251]})
+elements.update({"Es":["Einsteinium", 252]})
+elements.update({"Fm":["Fermium", 257]})
+elements.update({"Md":["Mendelevium", 258]})
+elements.update({"Lr":["Lawrencium", 262]})
+
+
+#adds an element to elements contained
+def add_element(ctu):
+  num_str = ""
+  while int(ctu) > -1:
+    num_str += compound[c - ctu]
+    ctu -= 1
+  repeats = int(num_str)
+  while repeats > 0:
+    elements_contained.append(current)
+    repeats -= 1
+
+#next two functions print the report of all the information we know
+def percent_of_total(element_looking_for, total):
+  times = num_of_each[element_looking_for]
+  mass = round(elements[element_looking_for][1] * times, 2)
+  return (round(((mass / total) * 100), 3), mass) #returns percent rounded to same number of decimal places as the other
+
+#prints the report at the end
+def print_report(percent_comp, total_mass):
+  print("---------------------------------------------------------")
+  print("| Total molar mass: {0:<36s}|".format(str(total_mass) + " grams/mole"))
+  print("|{0:<55s}|".format(" "))
+  print("|      {0:<49s}|".format("Percent Composition: "))
+
+  for el in num_of_each.keys():
+    #prints the percentage of each element and the respective mass
+    (percent, mass_of_element) = percent_of_total(el, total_mass)
+    if (len(num_of_each.keys()) == 1):
+      #19
+      print("|          {0:<13s}- {1:0>6.2f} percent {2:<15s}|".format(elements[el][0], percent, "(" + str(mass_of_element) + " grams)"))
+    else:
+      print("|          {0:<13s}- {1:0>6.3f} percent {2:<15s}|".format(elements[el][0], percent, "(" + str(mass_of_element) + " grams)"))
+
+  print("---------------------------------------------------------")
+
+while True:
+#gets compound and the elements in it
+  compound = input("Enter the compound: (Case sensitive)")
+  elements_contained = []
+  current = ""
+  quit = False
+  ct = 0
+  for c in range(0, len(compound)):
+    if (compound[c].isupper()):
+      #if it is uppercase, indicates that we have begun to look at a new element
+      current = compound[c]
+      try:
+        if ((not compound[c + 1].isdigit()) and (not compound[c + 1].islower())):
+          elements_contained.append(current)
+      except:
+        pass
+    elif (compound[c].islower()):
+      #if it's lower, add it to the existing element we're looking at
+      current += compound[c]
+      try:
+        if (compound[c + 1].isupper()):
+          elements_contained.append(current)
+      except:
+        pass
+    elif (compound[c].isdigit()):
+      try:
+        if (compound[c + 1].isdigit()):
+          #if there are more digits to the subscript then it counts that and moves on
+          ct += 1
+          continue
+        else:
+          #gets a string with a number that represents the coefficient no matter how many digits it has
+          add_element(ct)
+          ct = 0
+      except:
+        #means we're at the end of the number, so it appends it
+        add_element(ct)
+        ct = 0
+    else:
+      #prints invalid if it finds something that is a symbol like a $ or a #
+      print("Invalid symbol:", compound[c])
+      quit = True
+      break
+    #If it's the end of the compound, adds the final element the appropriate number of times
+    if ((c == len(compound) - 1) and not compound[c].isdigit()):
+      elements_contained.append(current)
+
+  molar_mass = 0
+  num_of_each = {}
+  #goes through each item in there and adds it to the total
+  if (not quit):
+    for ele in elements_contained:
+      if (ele in elements):
+        molar_mass += elements[ele][1]
+        if (ele in num_of_each):
+          num_of_each[ele] += 1
+        else:
+          num_of_each.update({ele:1})
+      else:
+        print("Invalid element:", ele)
+        quit = True
+        break
+
+  if (not quit):
+    num_places = input("How many places should the final answer be rounded to (extra zeros will be left off)? ")
+    try:
+      num_places = int(num_places)
+    except:
+      print("Invalid number of places, printing to 3 as default.")
+      num_places = 3
+
+    molar_mass = round(molar_mass, num_places)
+    print("") #blank line for clarity
+    print_report(num_of_each, molar_mass)
+
+  print("")
+  go_again = input("Calculate another (y/n)? ").lower()
+  if (go_again != "yes" and go_again != "y"):
+    print("\nThank you for using the molar mass calculator.")
+    break
+  else:
+    print("\n-----------------------------------------------------------------------------------------\n")
+    continue
